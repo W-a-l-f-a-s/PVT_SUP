8eb56d352a577c42563752a1fec18062a7392fa1
W-a-l-f-a-s <wayne.m.schwab@gmail.com>
2023-05-24 14:47:47 -0400

    spotti

--- data.py
+++ data.py
@@ -9,10 +9,9 @@ WEBSITE_LIST = {"https://www.flipkart.com/":
 		     },
 			"sub-endpoints":
 			{
-				"news" : {"randnews" : 'rand_ind~:css selector;article' , 'class name;read-more'
-					    },
-				"randtrenditem" : {'rand_ind~:css selector;div._1GTrm1',
-							 },
+				"news" : {"randnews" : 'rand_ind~:css selector;article'},
+                "randnews" : {"article" : 'class name;read-more'},
+				"randtrenditem" : {'rand_ind~:css selector;div._1GTrm1'},
 			 }
 		      }
 		     }
